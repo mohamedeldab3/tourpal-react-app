ef68576df090848ff08b019c72db656fad7a71c4
Mohamed Eldab3 <mohamed@example.com>
2025-09-26 06:31:14 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        'adminDashboard': './src/pages/dashboard/AdminDashboard.tsx',
-        'providerDashboard': './src/pages/dashboard/ProviderDashboard.tsx',
-        'userDashboard': './src/pages/dashboard/UserDashboard.tsx',
-        'profile': './src/pages/dashboard/Profile.tsx'
-      }
-    },
     chunkSizeWarningLimit: 800,
     sourcemap: true,
     minify: 'terser',
